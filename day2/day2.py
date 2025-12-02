faa6e2ba5493f67d1518980a851e877a682fea89
Nikolaj Engborg <Nikolaj Engborg>
2025-12-02 06:56:56 +0100

    First part done

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,6 @@
 import unittest
 
-from data import test_data, test_data2
+from data import test_data, test_data2, data
 
 
 class AnyClass:
@@ -9,20 +9,52 @@ class AnyClass:
 
 
 def parser(string: str):
-    string = string.split("\n")
+    string = string.split(",")
+    string = [start_stop.split("-") for start_stop in string]
+    for item in string:
+        item[0] = int(item[0])
+        item[1] = int(item[1])
+    return string
 
-    data_dict = {}
 
-    for row in string:
-        pass
+def number_intervals(list_of_intervals: list[int]) -> list[int]:
+    start = list_of_intervals[0]
+    end = list_of_intervals[1]
+    all_intervals = set()
+    for i in range(end - start):
+        all_intervals.add(start + i)
+    all_intervals.add(end)
+    return list(all_intervals)
 
 
-def do_the_thing(parsed_data):
-    pass
+def check_repeat(number_list: int):
+    repeat = 0
+    for number in number_list:
+        if len(str(number)) % 2 == 0:
+           if split_string_in_halfs(str(number)):
+               repeat += number
+    return repeat
+
+
+def split_string_in_halfs(string: str):
+    if len(string) % 2 == 0:
+        a = string[:len(string) // 2]
+        b = string[len(string) // 2:]
+        if a == b:
+            return int(a+b)
 
 
 def main():
-    print("Just testing")
+    intervals = parser(test_data)
+    true_intervals = []
+    total= 0
+    for interval in intervals:
+        true_intervals.append(number_intervals(interval))
+    for interval in true_intervals:
+        total += check_repeat(interval)
+    print("#######",total,"########")
+    #print(true_intervals)
+    #print(test_data2)
     pass
 
 
@@ -34,6 +66,7 @@ class test_day1(unittest.TestCase):
 
     def test_part1(self):
         self.assertTrue("This is a dummy test" == "This is a dummy test")
+        #print(parser(self.test_data))
 
     def test_part2(self):
         pass
